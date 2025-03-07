6a2ac13cbfcc4abaf68a8948faf286bfe5189e10
Gil Goldshlager <=>
2025-03-07 11:23:32 -0800

    Add explicit step

--- vmcnet/train/vmc.py
+++ vmcnet/train/vmc.py
@@ -163,7 +163,7 @@ def vmc_loop(
             utils.checkpoint.log_vmc_loop_state(epoch, metrics, checkpoint_str)
 
             if epoch % wandb_freq == 0:
-                wandb.log(metrics)
+                wandb.log(metrics, step=epoch)
 
             if nans_detected:
                 break
