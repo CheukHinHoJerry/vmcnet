37efa4c9571e5ceca4e222eb50342bc8a7e4b1dd
CheukHinHoJerry <cheukhinhojerry@gmail.com>
2025-04-07 03:47:18 +0100

    update example

--- chho_example.sh
+++ chho_example.sh
@@ -2,6 +2,9 @@ vmc-molecule \
     --config.problem.ion_pos="((0.0, 0.0, -2.0), (0.0, 0.0, 2.0))" \
     --config.problem.ion_charges="(7.0, 7.0)" \
     --config.problem.nelec="(7, 7)" \
-    --config.model.type="ACEwf" \
     --config.vmc.nburn=1 \
-    
+    --config.vmc.optimizer_type="warmsr" \
+    --config.vmc.optimizer.warmsr.srft_rank=300 \
+    --config.vmc.optimizer.warmsr.mu=0.99 \
+    --config.vmc.optimizer.warmsr.damping=0.001 \
+    --config.vmc.nchains=500 \
